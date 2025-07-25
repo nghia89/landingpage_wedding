eebf79fd58f6bb4be7bea3d0d7766412017f085b
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2025-07-25 12:31:58 +0700

    up

--- src/app/api-test/page.tsx
+++
@@ -1,9 +0,0 @@
-import ApiIntegrationExample from '@/components/ApiIntegrationExample';
-
-export default function ApiTestPage() {
-    return (
-        <div className="min-h-screen bg-gray-100 py-8">
-            <ApiIntegrationExample />
-        </div>
-    );
-}
