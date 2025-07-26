60438d9ca939515b08c8215390b93ff476dcc807
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2025-07-26 22:19:27 +0700

    up

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -7,11 +7,6 @@ export default async function AdminLayout({
 }: {
     children: React.ReactNode;
 }) {
-    const session = await getServerSession(authOptions);
-
-    if (!session) {
-        redirect('/admin/login');
-    }
 
     return <>{children}</>;
 }
